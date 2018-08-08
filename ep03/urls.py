89c9eb57764e685fef7508ad9a0a40eece09a656
min_2271 <min_2271@naver.com>
2018-08-08 16:28:23 +0900

    [ep03] ViewSet/Router 활용

--- ep03/urls.py
+++ ep03/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from . import views
 
 app_name = 'ep03'
 
+
+router = DefaultRouter()
+router.register(r'post', views.PostViewSet)
+router.register(r'user', views.UserViewSet)
+
 urlpatterns = [
-    path('post/', views.PostListAPIView.as_view()),
-    path('post/<int:pk>/', views.PostDetailAPIView.as_view()),
+    # path('post/', views.PostListAPIView.as_view()),
+    # path('post/<int:pk>/', views.PostDetailAPIView.as_view()),
+
+    path('', include(router.urls)),
+    # path('user/', views.user_list),
+    # path('user/<int:pk>/', views.user_detail),
 ]
--- ep03/views.py
+++ ep03/views.py
@@ -1,68 +1,32 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins, generics 
+from rest_framework import generics
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
-class PostListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# class PostListAPIView(generics.ListCreateAPIView):
     
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer 
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+# # /post/10/ => GET, PUT, DELETE
+# class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)    
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
 
-# class PostListAPIView(APIView):
+user_list = UserViewSet.as_view({
+    'get' : 'list',
+})
 
-#     def get(self, request):
-#         serializer = PostSerializer(Post.objects.all(), many=True)
-#         return Response(serializer.data)
-
-#     def post(self, request):
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=201)
-#         return Response(serializer.errors, status=400) 
-
-# /post/10/ => GET, PUT, DELETE
-class PostDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                        mixins.DestroyModelMixin, generics.GenericAPIView):
-
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-    def get(self, request, *args, **kwargs):
-        return self.retrive(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-# class PostDetailAPIView(APIView):
-#     def get_object(self, pk):
-#         return get_object_or_404(Post, pk=pk)
-
-#     def get(self, request, pk, format=None):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(post)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, pk):
-#         post = self.get_object(pk)
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+user_detail = UserViewSet.as_view({
+    'get' : 'retrieve',
+})

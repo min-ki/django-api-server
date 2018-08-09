22ce76248415b5c0c341312fd5c741d720804c7c
min_2271 <min_2271@naver.com>
2018-08-10 00:59:33 +0900

    EP07. Pagination 처리

--- ep03/views.py
+++ ep03/views.py
@@ -3,30 +3,13 @@ from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
+from .pagination import PostPageNumberPagination
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-# class PostListAPIView(generics.ListCreateAPIView):
-    
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer 
-
-# # /post/10/ => GET, PUT, DELETE
-# class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+    pagination_class = PostPageNumberPagination
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-user_list = UserViewSet.as_view({
-    'get' : 'list',
-})
-
-user_detail = UserViewSet.as_view({
-    'get' : 'retrieve',
-})

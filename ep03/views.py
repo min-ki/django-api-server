bc0862ced8d276c5d5a3c90133dd3f7c109bce15
min_2271 <min_2271@naver.com>
2018-08-10 13:47:35 +0900

    [ep08] Serializer를 통한 유효성 검사 및 저장

--- ep03/views.py
+++ ep03/views.py
@@ -13,3 +13,4 @@ class PostViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+   
